6610554fd4af9855cc327bfc24a25ebd7ab71c27
fcampelo <fcampelo@ufmg.br>
2016-05-21 20:32:59 -0300

    Added summary plot at the top
    Interactive(-ish) plot using rCharts

--- utils.R
+++ utils.R
@@ -2,7 +2,7 @@
 # by Felipe Campelo (fcampelo@ufmg.br ; http://github.com/fcampelo)
 
 # Load (and, if needed, install) required packages
-required.packages <- c("XML", "tools")
+required.packages <- c("XML", "tools", "devtools", "RColorBrewer")
 install_and_load_packages <- function(pkg){
     if (!(pkg %in% rownames(installed.packages()))){
         install.packages(pkg)
@@ -10,6 +10,10 @@ install_and_load_packages <- function(pkg){
     require(pkg, character.only = TRUE)
 }
 ignore <- lapply(required.packages, install_and_load_packages)
+
+# Installing from github
+if (!("rCharts" %in% rownames(installed.packages()))) install_github("ramnathv/rCharts")
+require(rCharts)
 
 # ==========================================
 # Function to easily capitalize author lists
@@ -102,10 +106,264 @@ ignore <- lapply(required.packages, install_and_load_packages)
 
 
 # ==========================================
-# Function to print accepted papers
+# Function to extract entries from a given year
 .selectyear <- function(x, year){
     return(x[x$Year == year, ])
 }
+
+
+
+# ==========================================
+# Function to extract and format accepted papers
+.getAccepted <- function(x){
+    npap     <- length(x$`PRODUCAO-BIBLIOGRAFICA`$`ARTIGOS-ACEITOS-PARA-PUBLICACAO`)
+    MyAccept <- data.frame(Authors = character(npap),
+                           Title   = character(npap),
+                           Journal = character(npap),
+                           DOI     = character(npap),
+                           Year    = character(npap),
+                           stringsAsFactors = FALSE)
+    if (npap){
+        for (i in 1:npap){
+            paper <- x$`PRODUCAO-BIBLIOGRAFICA`$`ARTIGOS-ACEITOS-PARA-PUBLICACAO`[[i]]
+            MyAccept$Title[i]   <- tools::toTitleCase(tolower(paper$`DADOS-BASICOS-DO-ARTIGO`[[2]]))
+            MyAccept$Journal[i] <- paper$`DETALHAMENTO-DO-ARTIGO`[[1]]
+            MyAccept$DOI[i]     <- ifelse(paper$`DADOS-BASICOS-DO-ARTIGO`[[9]] == "",
+                                          paste0("zNotAvailable no.", i),
+                                          paper$`DADOS-BASICOS-DO-ARTIGO`[[9]])
+            MyAccept$Year[i]    <- paper$`DADOS-BASICOS-DO-ARTIGO`[[3]]
+            authindx            <- which(names(paper) == "AUTORES")
+            nauthors            <- length(authindx)
+            MyAccept$Authors[i] <- .authorname(paper[[authindx[1]]][1])
+            if (nauthors > 1){
+                for (j in 2:nauthors){
+                    MyAccept$Authors[i] <- paste0(MyAccept$Authors[i], 
+                                                  ", ",
+                                                  .authorname(paper[[authindx[j]]][1]))
+                }
+            }
+        }
+    }
+    return(MyAccept)
+}
+
+
+
+# ==========================================
+# Function to extract and format published papers
+.getPapers <- function(x){
+    npap     <- length(x$`PRODUCAO-BIBLIOGRAFICA`$`ARTIGOS-PUBLICADOS`)
+    MyPapers <- data.frame(Authors = character(npap),
+                           Title   = character(npap),
+                           Journal = character(npap),
+                           Volume  = character(npap),
+                           Issue   = character(npap),
+                           Pages   = character(npap),
+                           Year    = character(npap),
+                           DOI     = character(npap),
+                           stringsAsFactors = FALSE)
+    
+    # Probably more effective if I used lapply, but more didactic with a for loop
+    if (npap){
+        for (i in 1:npap){
+            paper <- x$`PRODUCAO-BIBLIOGRAFICA`$`ARTIGOS-PUBLICADOS`[[i]]
+            MyPapers$Title[i]   <- tools::toTitleCase(tolower(paper$`DADOS-BASICOS-DO-ARTIGO`[[2]]))
+            MyPapers$Journal[i] <- paper$`DETALHAMENTO-DO-ARTIGO`[[1]]
+            MyPapers$Volume[i]  <- paper$`DETALHAMENTO-DO-ARTIGO`[[3]]
+            MyPapers$Issue[i]   <- ifelse(paper$`DETALHAMENTO-DO-ARTIGO`[[5]] == "", 
+                                          yes = "1",
+                                          no  = paper$`DETALHAMENTO-DO-ARTIGO`[[5]])
+            MyPapers$Pages[i]   <- paste0(paper$`DETALHAMENTO-DO-ARTIGO`[[6]],
+                                          "-",
+                                          paper$`DETALHAMENTO-DO-ARTIGO`[[7]])
+            MyPapers$Year[i]    <- paper$`DADOS-BASICOS-DO-ARTIGO`[[3]]
+            MyPapers$DOI[i]     <- ifelse(paper$`DADOS-BASICOS-DO-ARTIGO`[[9]] == "",
+                                          paste0("zNotAvailable no.", i),
+                                          paper$`DADOS-BASICOS-DO-ARTIGO`[[9]])
+            authindx            <- which(names(paper) == "AUTORES")
+            nauthors            <- length(authindx)
+            MyPapers$Authors[i] <- .authorname(paper[[authindx[1]]][1])
+            if (nauthors > 1){
+                for (j in 2:nauthors){
+                    MyPapers$Authors[i] <- paste0(MyPapers$Authors[i], 
+                                                  ", ",
+                                                  .authorname(paper[[authindx[j]]][1]))
+                }
+            }
+        }
+    }    
+    return(MyPapers)
+}
+
+
+
+# ==========================================
+# Function to extract and format conference papers
+.getConfs <- function(x){
+    npap    <- length(x$`PRODUCAO-BIBLIOGRAFICA`$`TRABALHOS-EM-EVENTOS`)
+    MyConfs <- data.frame(Authors    = character(npap),
+                          Title      = character(npap),
+                          Conference = character(npap),
+                          Country    = character(npap),
+                          DOI        = character(npap),
+                          Pages      = character(npap),
+                          Year       = character(npap),
+                          stringsAsFactors = FALSE)
+    
+    if (npap){
+        for (i in 1:npap){
+            paper <- x$`PRODUCAO-BIBLIOGRAFICA`$`TRABALHOS-EM-EVENTOS`[[i]]
+            MyConfs$Title[i]      <- tools::toTitleCase(tolower(paper$`DADOS-BASICOS-DO-TRABALHO`[[2]]))
+            MyConfs$Conference[i] <- paper$`DETALHAMENTO-DO-TRABALHO`[[2]]
+            MyConfs$Country[i]    <- paper$`DADOS-BASICOS-DO-TRABALHO`[[4]]
+            MyConfs$Year[i]       <- max(paper$`DETALHAMENTO-DO-TRABALHO`[[4]],
+                                         paper$`DADOS-BASICOS-DO-TRABALHO`[[3]])
+            MyConfs$Pages[i]      <- paste0(paper$`DETALHAMENTO-DO-TRABALHO`[[9]],
+                                            "-",
+                                            paper$`DETALHAMENTO-DO-TRABALHO`[[10]])
+            MyConfs$DOI[i]        <- ifelse(paper$`DADOS-BASICOS-DO-TRABALHO`[[9]] == "",
+                                            paste0("zNotAvailable no.", i),
+                                            paper$`DADOS-BASICOS-DO-TRABALHO`[[9]])
+            authindx              <- which(names(paper) == "AUTORES")
+            nauthors              <- length(authindx)
+            MyConfs$Authors[i]    <- .authorname(paper[[authindx[1]]][1])
+            if (nauthors > 1){
+                for (j in 2:nauthors){
+                    MyConfs$Authors[i] <- paste0(MyConfs$Authors[i], 
+                                                 ", ",
+                                                 .authorname(paper[[authindx[j]]][1]))
+                }
+            }
+        }
+    }
+    return(MyConfs)
+}
+
+
+
+# ==========================================
+# Function to extract and format book
+.getChaps <- function(x){
+    npap    <- length(x$`PRODUCAO-BIBLIOGRAFICA`$`LIVROS-E-CAPITULOS`$`CAPITULOS-DE-LIVROS-PUBLICADOS`)
+    MyChaps <- data.frame(Authors  = character(npap),
+                          Title    = character(npap),
+                          Bookname = character(npap),
+                          Volume   = character(npap),
+                          Pages    = character(npap),
+                          Year     = character(npap),
+                          DOI      = character(npap),
+                          stringsAsFactors = FALSE)
+    
+    if (npap){
+        for (i in 1:npap){
+            paper <- x$`PRODUCAO-BIBLIOGRAFICA`$`LIVROS-E-CAPITULOS`$`CAPITULOS-DE-LIVROS-PUBLICADOS`[[i]]
+            MyChaps$Title[i]    <- tools::toTitleCase(tolower(paper$`DADOS-BASICOS-DO-CAPITULO`[[2]]))
+            MyChaps$Bookname[i] <- tools::toTitleCase(tolower(paper$`DETALHAMENTO-DO-CAPITULO`[[1]]))
+            MyChaps$Volume[i]   <- paper$`DETALHAMENTO-DO-CAPITULO`[[2]]
+            MyChaps$Year[i]     <- paper$`DADOS-BASICOS-DO-CAPITULO`[[3]]
+            MyChaps$Pages[i]    <- paste0(paper$`DETALHAMENTO-DO-CAPITULO`[[3]],
+                                          "-",
+                                          paper$`DETALHAMENTO-DO-CAPITULO`[[4]])
+            MyChaps$DOI[i]      <- ifelse(paper$`DADOS-BASICOS-DO-CAPITULO`[[9]] == "",
+                                          paste0("zNotAvailable no.", i),
+                                          paper$`DADOS-BASICOS-DO-CAPITULO`[[9]])
+            authindx            <- which(names(paper) == "AUTORES")
+            nauthors            <- length(authindx)
+            MyChaps$Authors[i]  <- .authorname(paper[[authindx[1]]][1])
+            if (nauthors > 1){
+                for (j in 2:nauthors){
+                    MyChaps$Authors[i] <- paste0(MyChaps$Authors[i], 
+                                                 ", ",
+                                                 .authorname(paper[[authindx[j]]][1]))
+                }
+            }
+        }
+    }
+    return(MyChaps)   
+}
+
+
+
+# ==========================================
+# Function to extract and format books
+.getBooks <- function(x){
+    npap    <- length(x$`PRODUCAO-BIBLIOGRAFICA`$`LIVROS-E-CAPITULOS`$`LIVROS-PUBLICADOS-OU-ORGANIZADOS`)
+    MyBooks <- data.frame(Authors   = character(npap),
+                          Bookname  = character(npap),
+                          Publisher = character(npap),
+                          Edition   = character(npap),
+                          City      = character(npap),
+                          Pages     = character(npap),
+                          Year      = character(npap),
+                          ISBN      = character(npap),
+                          stringsAsFactors = FALSE)
+    
+    if (npap){
+        for (i in 1:npap){
+            paper <- x$`PRODUCAO-BIBLIOGRAFICA`$`LIVROS-E-CAPITULOS`$`LIVROS-PUBLICADOS-OU-ORGANIZADOS`[[i]]
+            MyBooks$Bookname[i]  <- tools::toTitleCase(tolower(paper$`DADOS-BASICOS-DO-LIVRO`[[3]]))
+            MyBooks$Publisher[i] <- paper$`DETALHAMENTO-DO-LIVRO`[[7]]
+            MyBooks$Edition[i]   <- paper$`DETALHAMENTO-DO-LIVRO`[[4]]
+            MyBooks$City[i]      <- paste0(paper$`DETALHAMENTO-DO-LIVRO`[[6]],
+                                           ", ", 
+                                           paper$`DADOS-BASICOS-DO-LIVRO`[[5]])
+            MyBooks$Pages[i]     <- paper$`DETALHAMENTO-DO-LIVRO`[[2]]
+            MyBooks$Year[i]      <- paper$`DADOS-BASICOS-DO-LIVRO`[[4]]
+            MyBooks$ISBN[i]      <- ifelse(paper$`DETALHAMENTO-DO-LIVRO`[[3]] == "",
+                                           paste0("zNotAvailable no.", i),
+                                           paper$`DETALHAMENTO-DO-LIVRO`[[3]])
+            authindx             <- which(names(paper) == "AUTORES")
+            nauthors             <- length(authindx)
+            MyBooks$Authors[i]   <- .authorname(paper[[authindx[1]]][1])
+            if (nauthors > 1){
+                for (j in 2:nauthors){
+                    MyBooks$Authors[i] <- paste0(MyBooks$Authors[i], 
+                                                 ", ",
+                                                 .authorname(paper[[authindx[j]]][1]))
+                }
+            }
+        }
+    }
+    
+    return(MyBooks)
+}
+
+
+
+# ==========================================
+# Function to plot summary chart
+.plotChart <- function(x){
+    myTotals           <- lapply(x, function(X){as.data.frame(table(X$Year))})
+    myTotals           <- do.call(rbind.data.frame, myTotals)
+    myTotals$ProdType  <- gsub("\\..*","",rownames(myTotals))
+    names(myTotals)[1] <- "Year"
+    myTotals$Year      <- as.numeric(as.character(myTotals$Year))
+    myTotals           <- myTotals[order(myTotals$Year), ]
+    myTotals2          <- with(myTotals, 
+                               expand.grid(Year     = unique(Year), 
+                                           ProdType = unique(ProdType)))
+    myTotals2$Freq    <- 0
+    
+    rownames(myTotals2) <- paste0(myTotals2$Year, myTotals2$ProdType)
+    rownames(myTotals)  <- paste0(myTotals$Year, myTotals$ProdType)
+    for (i in 1:nrow(myTotals)){
+        indx <- which(rownames(myTotals2) == rownames(myTotals)[i])
+        if(length(indx)){
+            myTotals2$Freq[indx] <- myTotals$Freq[i]
+        }
+    }
+    
+    TotalsPlot <- nPlot(Freq ~ Year, 
+                        group = "ProdType", 
+                        data  = myTotals2, 
+                        type  = "multiBarChart")
+    TotalsPlot$chart(color        = RColorBrewer::brewer.pal(8, "Set1"), 
+                     reduceXTicks = FALSE)
+    TotalsPlot$xAxis(rotateLabels=-90)
+    TotalsPlot$set(width = 800)
+    TotalsPlot$print('iframesrc', cdn = TRUE, include_assets = TRUE)
+}
+
 
 
 
@@ -246,7 +504,7 @@ ignore <- lapply(required.packages, install_and_load_packages)
         for (i in 1:nrow(x)){
             cat(i, ". ",
                 x$Authors[i],
-                ": **", x$Bookname[i], "**. ",
+                ": _", x$Bookname[i], "_. ",
                 sep = "")
             if(x$Publisher[i] != ""){
                 cat(x$Publisher[i], ", ",
