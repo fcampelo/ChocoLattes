f2d53e4e446588f9b4109f2aa731c3879bd623e4
Felipe Campelo <fcampelo@ufmg.br>
2017-04-12 18:31:37 -0300

    More work on plot_charts

--- R/plot_chart.R
+++ R/plot_chart.R
@@ -1,49 +1,51 @@
 # Function to plot summary chart
 
-# TO DO
+plot_chart <- function(lattes.list, type = "ggplot2"){
+  # preprocess lattes.list
+  lattes.list$`Conference Papers - International` <- lattes.list$`Conference Papers`[which(lattes.list$`Conference Papers`$Internac), ]
+  lattes.list$`Conference Papers - National`      <- lattes.list$`Conference Papers`[which(!lattes.list$`Conference Papers`$Internac), ]
+  lattes.list$`Conference Papers`                 <- NULL
 
-plot_chart <- function(x, type = "rcharts"){
-  x$ConfsIntl       <- x$Confs[which(x$Confs$Internac), ]
-  x$ConfsNac        <- x$Confs[which(!x$Confs$Internac), ]
-  x$Confs            <- NULL
-  myTotals           <- lapply(x, function(X){as.data.frame(table(X$Year))})
-  myTotals           <- do.call(rbind.data.frame, myTotals)
-  myTotals$Type      <- gsub("\\..*","",rownames(myTotals))
-  names(myTotals)    <- c("Year", "Count", "Type")
-  myTotals$Year      <- as.numeric(as.character(myTotals$Year))
-  myTotals           <- myTotals[order(myTotals$Year), ]
+  # Assemble data frame with total counts per year
+  myTotals              <- lapply(lattes.list,
+                                  function(x){as.data.frame(table(x$Year))})
+  myTotals              <- do.call(rbind.data.frame, myTotals)
+  myTotals$Type         <- gsub("\\..*","", rownames(myTotals))
+  names(myTotals)       <- c("Year", "Count", "Type")
+  myTotals$Year         <- as.numeric(as.character(myTotals$Year))
+  myTotals              <- myTotals[order(myTotals$Year), ]
+  rownames(myTotals)    <- paste0(myTotals$Year, myTotals$Type)
+
+  # Second data frame containing all entries by year (even if with 0 occurrences)
   myTotals2          <- with(myTotals,
                              expand.grid(Year     = unique(Year),
                                          Type     = unique(Type)))
   myTotals2$Count    <- 0
-
   rownames(myTotals2) <- paste0(myTotals2$Year, myTotals2$Type)
-  rownames(myTotals)  <- paste0(myTotals$Year, myTotals$Type)
+
+  # Populate counts for myTotals2
   for (i in 1:nrow(myTotals)){
     indx <- which(rownames(myTotals2) == rownames(myTotals)[i])
     if(length(indx)){
       myTotals2$Count[indx] <- myTotals$Count[i]
     }
   }
-  levels(myTotals2$Type) <- c("Art. Conferências Internacionais",
-                              "Art. Periódicos",
-                              "Art. Conferências Nacionais/Regionais",
-                              "Livros",
-                              "Capítulos de Livros",
-                              "Artigos Aceitos para Publicação")
+
 
   ## Using ggplot2
   if (type == "ggplot2" | type == "ggplotly"){
-    myPlot <- ggplot(data    = myTotals2,
-                     mapping = aes(x     = Year,
-                                   y     = Count,
-                                   fill  = Type)) +
-      geom_bar(stat     = "identity",
-               position = "stack",
-               colour   = "#00000011") +
-      scale_x_continuous(breaks = min(myTotals2$Year):max(myTotals2$Year)) +
-      theme(axis.text.x     = element_text(angle = 45, vjust = 0),
-            legend.position = "bottom")
+    myPlot <- ggplot2::ggplot(data    = myTotals2,
+                              mapping = ggplot2::aes(x     = Year,
+                                                     y     = Count,
+                                                     fill  = Type)) +
+      ggplot2::geom_bar(stat     = "identity",
+                        position = "stack",
+                        colour   = "#00000011") +
+      ggplot2::scale_x_continuous(breaks = min(myTotals2$Year):max(myTotals2$Year)) +
+      ggplot2::theme(axis.text.x     = ggplot2::element_text(angle = 45, vjust = 0),
+                     legend.position = "bottom")
+
+    # STOPPED HERE
     if (type == "ggplotly"){
       myPlot <- ggplotly(myPlot, width = 960, height = 480)
     }
